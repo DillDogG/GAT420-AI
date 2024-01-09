1247f635640c15a4773dd97472201310c2942236
DillDogG <dilldoggaming@gmail.com>
2024-01-08 21:19:01 -0700

    Cells update

--- Src/A-Life/Cells.cpp
+++ Src/A-Life/Cells.cpp
@@ -11,6 +11,26 @@ bool Cells::Initialize()
 void Cells::Step()
 {
 	// "draw" onto buffer
+	Write<uint8_t>(buffer, (size.x / 2), (size.y - 1), 1);
+	// rule 30 = 00011110 (binary)
+	uint8_t rule[] = { 0, 1, 1, 1, 1, 0, 0, 0 };
+	// update buffer
+	for (int y = 0; y < size.y - 1; y++)
+	{
+		for (int x = 0; x < size.x; x++)
+		{
+			uint8_t i = 0;
+			// read surrounding cells (x-1, x, x+2), a value between 0-7 can be created by shifting the bits
+			i |= Read<uint8_t>(buffer, x - 1, y) << 2;
+			i |= Read<uint8_t>(buffer, x, y) << 1;
+			i |= Read<uint8_t>(buffer, x + 1, y) << 0;
+
+			// elementary cellular automata rules
+			//uint8_t state = (rule & 1 << i) ? 1 : 0;
+			uint8_t state = (rule[7 - i]) ? 1 : 0;
+			Write<uint8_t>(buffer, x, (y + 1), state);
+		}
+	}
 	if (mouse_button[1])
 	{
 		Write<uint8_t>(buffer, mouse_position.x, mouse_position.y, 255);
